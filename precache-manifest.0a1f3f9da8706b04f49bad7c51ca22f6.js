8c0ff9ee206d4d37bebf3036c6053ab8f00770fd
mendesbayout <62385787+mendesbayout@users.noreply.github.com>
2020-10-06 17:43:13 -0300

    V1.4.1

---
+++ precache-manifest.0a1f3f9da8706b04f49bad7c51ca22f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16624b6680beef368417560e5ee01f3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f53dc9016c39f269d456",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bb0f8fb75697f537ed0a",
+    "url": "/static/css/main.7b8822e0.chunk.css"
+  },
+  {
+    "revision": "f53dc9016c39f269d456",
+    "url": "/static/js/2.ad959f2b.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.ad959f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0f8fb75697f537ed0a",
+    "url": "/static/js/main.fe583c8f.chunk.js"
+  },
+  {
+    "revision": "c83a45c6edf3bef28cc5",
+    "url": "/static/js/runtime-main.d2fb79cb.js"
+  },
+  {
+    "revision": "3be8c297162e0a4c27e1f63b2bca0513",
+    "url": "/static/media/github-desktop.3be8c297.jpg"
+  }
+]);
